cb0e3cbaf0f2db994c0a8fde5b3c394bb6ae6065
Olayinka Akeju <olayinka@eurocomci.co.uk>
2023-09-25 02:29:09 +0100

    Generating Libraries

--- apps/doggo-rating-app/src/main.ts
+++ apps/doggo-rating-app/src/main.ts
@@ -1,7 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { environment } from '@doggo-rating-workspace/shared/util-environments';
+import { enableProdMode } from '@angular/core';
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
 );
+
+if (environment.production) {
+  enableProdMode();
+}
